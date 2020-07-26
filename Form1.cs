c3da1aa18d0d8a31dea872620b6808c3b110457d
柏榮 <pool00721@hotmail.com.tw>
2020-07-26 18:45:23 +0800

    測試2

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace GitTest
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("GitTest");
-
+            MessageBox.Show("GitTest2");
         }
 
         private void Form1_Load(object sender, EventArgs e)
